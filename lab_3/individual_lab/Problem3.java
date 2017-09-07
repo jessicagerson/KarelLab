a11596c678299c2cb2851905df3c52bf9be533ea
jessicagerson <jessygerson123@gmail.com>
2017-09-07 12:38:19 -0700

    Karel Lab 3 Day 2

--- lab_3/individual_lab/Problem3.java
+++ lab_3/individual_lab/Problem3.java
@@ -3,7 +3,7 @@ package lab_3.individual_lab;
 import kareltherobot.*;
 import java.awt.Color;
 
-public class Problem3 extends Robot
+public class Problem3 extends Problem2
 {
    
     /**
@@ -12,10 +12,74 @@ public class Problem3 extends Robot
     public Problem3(int st, int av, Direction dir, int beep)
     {
         super(st, av, dir, beep);
+}
+public void carpetRooms(){
+    move();
+    turnLeft();
+    move();
+    carpetTime();
+    carpetTime();
+    carpetTime();
+}
+    public void carpetTime(){
+    
+    if (((!rightIsClear() && !leftIsClear()) && topIsClear()) == true){
+        move();
+        if (((!rightIsClear() && !leftIsClear()) && topIsClear()) == true){
+            move();
+            if (((!rightIsClear() && !leftIsClear()) && !topIsClear()) == true){
+                turnLeft();
+                turnLeft();
+                putBeeper();
+                move();
+                putBeeper();
+                move();
+                putBeeper();
+            }
+        }
     }
-
-    public void carpetRooms(){
-        
+    else{
+        if (((!rightIsClear() && !leftIsClear()) && !topIsClear()) == true){
+            putBeeper();
+            turnLeft();
+            turnLeft();
+            move();
+            
+        }
+    }
+    turnLeft();
+    move();
+    turnLeft();
+    move();
+} 
+public boolean leftIsClear(){
+    turnLeft();
+    if (frontIsClear()){
+        return true;
+    }
+    else{
+        return false;
     }
 }
+public boolean rightIsClear(){
+    turnRight();
+    if (frontIsClear()){
+        return true;
+    }
+    else{
+        return false;
+    }
+}
+public boolean topIsClear(){
+    
+    if (frontIsClear()){
+        return true;
+    }
+    else{
+        return false;
+    }
+}
+}
+
+
 
